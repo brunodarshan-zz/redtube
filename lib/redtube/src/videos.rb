98db9678fb4514eacebd0f7f600cbea078f9eab3
brunodarshan <brunodarshan@gmail.com>
2017-11-05 14:53:45 -0200

    change method embed

--- lib/redtube/src/videos.rb
+++ lib/redtube/src/videos.rb
@@ -1,15 +1,24 @@
 require 'redtube/redtube'
 
 module Redtube
-
    class Videos
+    #  Set of methods to request info about vídeos from YoupornAPI
+    #
+    # Example:
+    #   >> params = {category: "lorem", page 3}
+    #   >> videos =  Redtube::Videos.new
+    #   >> videos.search params
+
      include Redtube::Base
 
      def search(params)
        @@params.merge!(params)
        run_to_api "redtube.Videos.searchVideos"
      end
-
+#   Method self.get_video
+#   >> id = 1234
+#   >> video.get_video id
+#   => {"video"=>{"duration"=>"1:18", "views"=>2974098 ...
      def get_video(id, thumbsize = "all")
        @@params[:video_id] = id
        @@params[:thumbsize] = thumbsize
@@ -25,12 +34,16 @@ module Redtube
          return false
        end
      end
-
+#    Method embed
+#     Return the code to embed source video
+#     >> v.embed  id=1
+#     => {"embed"=>{"code"=>"PGlmcmFtZSBzcmM9Imh0dHBzOi8vZW1iZWQucmVkdHViZS5jb20vP2lkP....
      def embed(id)
-       if self.is_active? id
+       if is_active? id
          run_to_api "redtube.Videos.getVideoEmbedCode"
+       else
+         return false
        end
-       return false
      end
 
   # End class
